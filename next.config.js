510a52ac332e2901182ceb1c30dbe69372daf41a
slimo-group <mikehao@slimo.tech>
2025-04-17 15:59:03 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// This is an empty config file to prevent Next.js from auto-detecting
+module.exports = {
+  // This is a static HTML site, no Next.js required
+  // This file is only here to prevent auto-detection
+  reactStrictMode: true,
+  skipMiddlewareUrlNormalize: true,
+  skipTrailingSlashRedirect: true,
+} 
